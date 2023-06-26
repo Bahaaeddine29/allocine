8cd07ca80c0fed2a3a9c40715e8a6a192f8be7a1
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-06-26 14:08:46 +0200

    initialecommit

---
+++ src/Service/PDOservice.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\Service; 
+
+use PDO; 
+
+class PDOservice 
+{
+    protected PDO $pdo;
+
+    private string $dsn = 'mysql:host=127.0.0.1:3306;dbname=allocine';
+    private string $user = 'root';
+    private string $pwd = ' '; 
+
+
+
+}
